55ea7f70b5629c677d8cf3c3686f4a695643dcb9
ivan.sakharov <teleperedozik@mail.ru>
2023-06-29 08:04:34 +0300

    sdfg

---
+++ Module-8/8.6.2.cs
@@ -0,0 +1,28 @@
+﻿namespace DirectoryManager
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RecursiveSizeCalculator("D:\\temp\\testfolder"));
+        }
+
+        public static long RecursiveSizeCalculator(string folderPath)
+        {
+            long size = 0;
+            var dirInfo = new DirectoryInfo(folderPath);
+            foreach (FileInfo file in dirInfo.GetFiles())
+            {
+                size += file.Length;
+            }
+            if (dirInfo.GetDirectories().Count() > 0)
+            {
+                foreach (DirectoryInfo dir in dirInfo.GetDirectories())
+                {
+                    size += RecursiveSizeCalculator(dir.FullName);
+                }
+            }
+            return size;
+        }
+    }
+}
